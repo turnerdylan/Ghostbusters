82a2f532b29d652ff7ff0cf71b214bc248cb94fd
turnerdylan <dytu4626@colorado.edu>
2020-12-24 09:07:42 -0700

    Revert "Revert "new prototype!""
    This reverts commit ca5cde0238174a78b774dbd593ead30c5b75381f.

---
+++ Ghostbusters/Assets/MediumGhost.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MediumGhost : MonoBehaviour
+{
+    private void Start()
+    {
+        GhostManager.Instance.mediumGhosts.Add(gameObject);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.wKey.wasPressedThisFrame)
+        {
+            
+        }
+
+    }
+}
