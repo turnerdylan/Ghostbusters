cf7dfe87015b9de03cd6423fbb0d86e90232175f
zSpaceSheikh <50639756+zSpaceSheikh@users.noreply.github.com>
2021-04-14 10:08:28 -0600

    Updates 4/13

---
+++ Ghostbusters/Assets/Third Party/ParticlesThatPop/scripts/EmitParticles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitParticles : MonoBehaviour {
+
+	private ParticleSystem TheParticles;
+	// Use this for initialization
+	void Start () {
+		TheParticles = GetComponentInChildren<ParticleSystem>();
+
+		if(TheParticles == null)
+		{
+			Debug.Log("No Particles Attached. Drag a particle system from Prefabs onto the game object named TheHolder.");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void EmitSomeParticles()
+	{
+		int HowManyParticles = 144; // change this number for more or less particles.
+		TheParticles.Emit(HowManyParticles);
+	}
+}
